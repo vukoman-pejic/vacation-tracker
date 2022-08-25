a106f1ef743a45ca204f5c73eeb8e9e907fdb0cd
Vukoman Pejic <vukpejic4@gmail.com>
2022-08-25 19:23:54 +0200

    app init

---
+++ src/main/java/com/rbt/vacationtracker/VacationTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.rbt.vacationtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbt/vacationtracker/VacationTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbt.vacationtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
